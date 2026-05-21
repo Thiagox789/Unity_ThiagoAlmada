f76a250babf14f72bcf69183e5285ee3f104cff1
Thiagox789 <almadathiagoignacio@gmail.com>
2026-05-20 22:01:44 -0300

    Examen3-Entrega2.2

--- Examen3/Assets/Scripts/GameController.cs
+++ Examen3/Assets/Scripts/GameController.cs
@@ -1,16 +1,81 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public UIController componenteUI; 
+    public Slider sliderDificultad;   
+
+    public float tiempoDeJuego = 30f;
+    public int puntosParaGanar = 3;
+    private int puntosActuales = 0;
+    private bool juegoTerminado = false;
+    private bool juegoEmpezado = false; 
+
     void Start()
     {
-        
+        if (sliderDificultad != null)
+        {
+            ActualizarReglasPorSlider(sliderDificultad.value);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!juegoEmpezado || juegoTerminado) return;
+
+        if (tiempoDeJuego > 0)
+        {
+            tiempoDeJuego -= Time.deltaTime;
+            componenteUI.MostrarDatosEnPantalla(tiempoDeJuego, puntosActuales, puntosParaGanar);
+
+            if (tiempoDeJuego <= 0)
+            {
+                tiempoDeJuego = 0;
+                EvaluarFinDePartida();
+            }
+        }
+    }
+
+    public void ActualizarReglasPorSlider(float valorSlider)
+    {
+        if (juegoEmpezado) return;
+
+        puntosParaGanar = 3 + Mathf.RoundToInt(valorSlider * 5f); 
+        componenteUI.MostrarDatosEnPantalla(tiempoDeJuego, puntosActuales, puntosParaGanar);
+    }
+
+    public void PresionarBotonPrincipal()
+    {
+        if (juegoTerminado)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            return;
+        }
+
+        if (!juegoEmpezado)
+        {
+            juegoEmpezado = true;
+            if (sliderDificultad != null)
+            {
+                sliderDificultad.interactable = false;
+            }
+        }
+
+        puntosActuales++;
+        componenteUI.MostrarDatosEnPantalla(tiempoDeJuego, puntosActuales, puntosParaGanar);
+
+        if (puntosActuales >= puntosParaGanar)
+        {
+            componenteUI.CambiarColorMarcador(Color.green);
+        }
+    }
+
+    void EvaluarFinDePartida()
+    {
+        juegoTerminado = true;
+        bool gano = puntosActuales >= puntosParaGanar;
+        componenteUI.MostrarPantallaFinal(gano);
     }
 }
--- Examen3/Assets/Scripts/UIController.cs
+++ Examen3/Assets/Scripts/UIController.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TextMeshProUGUI textoTiempo;    
+    public TextMeshProUGUI textoPuntaje;   
+    public TextMeshProUGUI textoBoton;     
+    public GameObject cartelGanaste;       
+    public GameObject cartelDerrota;      
+
+    public void MostrarDatosEnPantalla(float tiempo, int puntos, int meta)
     {
-        
+        if (textoTiempo != null) 
+            textoTiempo.text = "Tiempo Restante: " + Mathf.CeilToInt(tiempo) + "s";
+            
+        if (textoPuntaje != null) 
+            textoPuntaje.text = "Puntaje: " + puntos + "/" + meta;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MostrarPantallaFinal(bool gano)
     {
-        
+        if (gano && cartelGanaste != null) cartelGanaste.SetActive(true);
+        if (!gano && cartelDerrota != null) cartelDerrota.SetActive(true);
+
+        if (textoBoton != null) textoBoton.text = "¡Reiniciar!";
+    }
+
+    public void CambiarColorMarcador(Color colorNuevo)
+    {
+        if (textoPuntaje != null) textoPuntaje.color = colorNuevo;
     }
 }
